ea3d7ee9abb1463f6ddee099278ce358edc90f86
tommy-swimmer <tmswimmer@fortlewis.edu>
2020-11-30 00:49:40 -0700

    command examples

--- new_bot.py
+++ new_bot.py
@@ -14,12 +14,15 @@ TOKEN = open("token.txt","r").readline()
 client = commands.Bot(command_prefix = '.') # Establishing the prefix
 # We'll mention the prefix later
 
-# Answers with ms latency
+######################################## Some Commands ########################################
+########################################               ########################################
 
+# Prints Bot is ready, if successful, in terminal.
 @client.event
 async def on_ready():
     print('Bot is ready')
 
+# Answers with ms latency
 @client.command()
 async def ping(ctx):    # Establishing the context for the prefix
     await ctx.send(f'Pong! {round(client.latency * 1000)}ms ') # Sending Pong! and Latency in ms
@@ -29,10 +32,17 @@ async def gg(ctx):    # Establishing the context for the prefix as 'gg'
     await ctx.send(f'gg') # Reply with gee gee
 
 @client.command()
+async def google(ctx):
+    await ctx.send("What would you like to search for?")
+
+@client.command()
 async def quote(ctx):
     responses = open('quotes.txt').read().splitlines()
     random.seed(a=None)
     response = random.choice(responses)
     await ctx.send(response)
+
+################################################################################
+################################################################################
 
 client.run(TOKEN)
